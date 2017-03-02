5253afc51f5f3805c9a8fc71d330d634825278ed
Callum Hewitt <callumhewitt95@gmail.com>
2017-03-02 15:42:53 +0000

    Added SimSetup GUI code

---
+++ src/main/java/aim4/sim/setup/merge/S2SSimSetup.java
@@ -0,0 +1,15 @@
+package aim4.sim.setup.merge;
+
+import aim4.sim.Simulator;
+import com.sun.scenario.effect.Merge;
+
+/**
+ * Created by Callum on 02/03/2017.
+ */
+public class S2SSimSetup implements MergeSimSetup {
+
+    @Override
+    public Simulator getSimulator() {
+        return null;
+    }
+}
